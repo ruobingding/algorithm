80094bd5ed9b91917828070a4e78abd910c73930
Robin <robin@RobindeMacBook-Pro.local>
2020-09-10 17:57:11 +0800

    Add simple array list

---
+++ src/test/java/com/robin/algorithm/datastructures/list/SimpleArrayListTest.java
@@ -0,0 +1,48 @@
+package com.robin.algorithm.datastructures.list;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleArrayListTest {
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void removeAt() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+}
